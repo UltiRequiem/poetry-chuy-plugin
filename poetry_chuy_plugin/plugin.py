2b153e71c3c5d51705eb81f2cc269566b09bfc64
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-18 19:53:55 -0500

    :sparkles: Test Poetry API

---
+++ poetry_chuy_plugin/plugin.py
@@ -0,0 +1,23 @@
+try:
+    from poetry.console import application as app
+    from poetry.plugins.application_plugin import ApplicationPlugin
+except ModuleNotFoundError:
+    app = type("application", (), {"Application": None})
+    ApplicationPlugin = type(
+        "ApplicationPlugin",
+        (),
+        {"PLUGIN_API_VERION": "1.0.0", "type": "application.plugin"},
+    )
+
+
+class PoePlugin(ApplicationPlugin):
+    def activate(self, application: app.Application) -> None:
+
+        if app.Application is None:
+            print(
+                "Poetry not found. Did you install this plugin via `poetry plugin add poetry-chuy-plugin`?"
+            )
+            sys.exit(1)
+
+        print("Is Working!")
+        print(application)
